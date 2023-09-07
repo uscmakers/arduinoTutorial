698b98390653135011dc97526c882e6a4ad13300
liyvonne <79174603+liyvonne@users.noreply.github.com>
2023-09-07 15:15:33 -0700

    Update potLED.ino

--- potLED/potLED.ino
+++ potLED/potLED.ino
@@ -1,20 +1,20 @@
-const int POT_PIN = 16;
-const int LED_PIN = 6;
+const int POT_PIN = 19;
+const int LED_PIN = 3;
 int potValue;
 int ledValue;
 
 void setup() {
   pinMode(POT_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
-
   Serial.begin(9600);
+  
 }
 
 void loop() {
-  // Note: analogRead only reads values from 0 to 4095
+  // Note: analogRead only reads values from 0 to 1023 for Arduino Uno
   potValue = analogRead(POT_PIN);
   // Convert potValue to ledValue since analogWrite only writes values from 0 to 255
-  ledValue = float(potValue)/4095*255;
+  ledValue = float(potValue)/1023*255;
   
   // Print potValue and ledValue for troubleshooting assistance
   Serial.println("potValue = " + String(potValue) + ", ledValue = " + String(ledValue));
