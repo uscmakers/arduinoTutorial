88db5ef3d196d8db0e267e31d555a784e3731e8c
liyvonne <79174603+liyvonne@users.noreply.github.com>
2023-09-07 15:16:16 -0700

    LED brightness changes with Ultrasonic Sensor distance value

--- ultrasonicSensor/ultrasonicSensor.ino
+++ ultrasonicSensor/ultrasonicSensor.ino
@@ -1,25 +1,38 @@
+const int TRIG_PIN = 4;
+const int ECHO_PIN = 5;
+const int LED_PIN = 3;
 
-const int trigPin = 9;
-const int echoPin = 10;
-
+int ledValue;
 float duration, distance;
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
+  // Transmit ultrasonic wave
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
+  // Calculate distance based on time between sending and receiving signal
+  duration = pulseIn(ECHO_PIN, HIGH);
   distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  
+  // Convert distance to LED brightness value, ensuring the range 0-255
+  ledValue = 255 - distance/90*255;
+  if (ledValue >= 0){
+    analogWrite(LED_PIN, ledValue);
+  } else{
+    analogWrite(LED_PIN, 0);
+  }
+ 
+  // Print distance and ledValue for debugging
+  Serial.println("distance = " + String(distance) + ", ledValue = " + String(ledValue));
+  
   delay(100);
 }
